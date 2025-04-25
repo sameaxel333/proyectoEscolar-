73a0305766f0286fec8fc54b6d72c4588982eeb6
sameaxel332 <sameaxel33@gmail.com>
2025-04-24 20:46:02 -0600

    commit inicial

---
+++ ProyectoEscolarizado/ProyectoEscolarizado/Model/IUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEscolarizado.Model
+{
+   public interface IUserRepository
+    {
+        bool AuthenticateUser(NetworkCredential credential);
+
+        void Add(UserModel userModel);
+
+        void Edit(UserModel userModel);
+
+        void Remove(int id );
+
+        UserModel GetByID(int id);
+
+        UserModel GetByUsername(string username);
+
+
+    }
+}
---
+++ ProyectoEscolarizado/ProyectoEscolarizado/View/Inscripciones .xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoEscolarizado.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class Inscripciones : Window
+    {
+        public Inscripciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtApellidos_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
