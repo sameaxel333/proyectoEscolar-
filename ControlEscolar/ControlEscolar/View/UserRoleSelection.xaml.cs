90e70b20c62fac168b4325387f0a9b632e34db56
SebAcunA <eduardo.sebas.acuna.lujan@gmail.com>
2025-05-04 00:01:44 -0600

    holi agregar archivo userSelectionRol y cambio a la contraseña etc etc

---
+++ ControlEscolar/ControlEscolar/View/UserRoleSelection.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ControlEscolar.View;
+using System.Windows;
+
+namespace ProyectoEscolarizado.View
+{
+    public partial class UserRoleSelection : Window
+    {
+        public UserRoleSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void Estudiante_Click(object sender, RoutedEventArgs e)
+        {
+            AbrirLogin("Estudiante");
+        }
+
+        private void Maestro_Click(object sender, RoutedEventArgs e)
+        {
+            AbrirLogin("Maestro");
+        }
+
+        private void Admin_Click(object sender, RoutedEventArgs e)
+        {
+            AbrirLogin("Administrador");
+        }
+
+        private void AbrirLogin(string tipoUsuario)
+        {
+            // Aquí puedes pasar el tipo de usuario al ViewModel si lo necesitas
+            var login = new LoginView(); // Asumiendo que LoginView recibe esto
+            login.Show();
+            this.Close();
+        }
+    }
+}
