b8dd7da6dd234d882af3cf3169f610af8cedcdcd
XxHectorValenciagaxX <vladimirweber117@gmail.com>
2025-05-11 02:47:06 -0600

    Add files via upload
    Añadí el registro de esos usuarios

---
+++ ControlEscolar/ControlEscolar/MoreWindows/StudentRegister.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ControlEscolar.MoreWindows
+{
+    /// <summary>
+    /// Lógica de interacción para StudentRegister.xaml
+    /// </summary>
+    public partial class StudentRegister : Window
+    {
+        public StudentRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
